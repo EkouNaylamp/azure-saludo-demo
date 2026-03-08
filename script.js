ceda420bfd0f43882626ed4be0899f3c62153d9a
EkouNaylamp <115675532+EkouNaylamp@users.noreply.github.com>
2026-03-08 18:47:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,12 @@
-async function saludar(){
+async function saludar() {
 
-const nombre = document.getElementById("nombre").value;
+    const nombre = document.getElementById("nombre").value;
 
-if(!nombre){
-alert("Ingrese su nombre");
-return;
-}
+    const respuesta = await fetch(
+        "https://saludo-function-josue-cuggf6g3ggczhmd4.canadacentral-01.azurewebsites.net/api/Saludo?nombre=" + nombre
+    );
 
-const url = "https://TU-FUNCTION.azurewebsites.net/api/saludo?nombre=" + nombre;
+    const texto = await respuesta.text();
 
-try{
-
-const response = await fetch(url);
-
-const data = await response.json();
-
-document.getElementById("resultado").innerText = data.mensaje;
-
-}catch(error){
-
-document.getElementById("resultado").innerText = "Error al conectar con la API";
-
-}
-
+    document.getElementById("resultado").innerText = texto;
 }
